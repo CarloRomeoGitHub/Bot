aeefd49230e5785545b0c000ce22cf4643317e65
CarloRomeoGitHub <59288553+CarloRomeoGitHub@users.noreply.github.com>
2021-06-24 11:44:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ def main():
   # on different commands - answer in Telegram
   dispatcher.add_handler(CommandHandler("start", start))
   dispatcher.add_handler(CommandHandler("help", help_command))
-  dispatcher.add_error_handler(CallbackContext)
+#   dispatcher.add_error_handler(CallbackContext)
   # on noncommand i.e message - echo the message on Telegram
   dispatcher.add_handler(MessageHandler(Filters.photo, photo)) 
 
